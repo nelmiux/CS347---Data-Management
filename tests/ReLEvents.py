096a5382bdd3ed4db1c3450b61e95ac1585012d4
Marek <marek.bejda@gmail.com>
2015-11-22 11:46:33 -0600

    total reset

--- tests/ReLEvents.py
+++
@@ -1,4 +0,0 @@
-global_conn = connectTo 'jdbc:oracle:thin:@rising-sun.microlab.cs.utexas.edu:1521:orcl' 'CS347_prof' 'orcl_prof' 'rdf_mode' 'EVENTS'
-results = SQL on global_conn """select * from EVENTS where EventType='AUTHENTICATION_FAILED_EVENT'"""
-for i in results :
-   print "RESULTS: ", i
--- tests/ReLGetExpNums.py
+++
@@ -1,3 +0,0 @@
-connectTo 'jdbc:oracle:thin:@ib-perfdb.us.oracle.com:1525:perfdb' 'cannata' 'orcl' 'remote';
-for i in range(1,65) :
-   SQL """ insert into ibexperiment(name, exp_date, tool, protocol, published) values ('BM  on cloud-mwm-15 -> cloud-mwm-16 S12b23 1 stream only, multi-parts """ i """/ 64', to_char(sysdate), 'rds-stress', 'IPoIB', 'Yes');"""
