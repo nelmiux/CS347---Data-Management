6f22640d55c3ef5602025b43b3ced581f351f68e
Marek <marek.bejda@gmail.com>
2015-11-22 12:05:51 -0600

    Removing everything against

--- README.installFlask.txt
+++
@@ -1,8 +0,0 @@
-ant clean
-move dist to the trash
-ant
-ant
-sudo pip install --target=/Users/pcannata/Mine/MyReL/dist/Lib/ Flask
-dist/bin/jython RestfulReL/mike68561-restful-rel-ea5e3cef544e/restful_start.py
-
-sudo chown pcannata /Users/pcannata/Mine/MyReL/dist/Lib/
--- tests/plsql.py
+++
@@ -1,7 +0,0 @@
-connectTo 'jdbc:oracle:thin:@rising-sun.microlab.cs.utexas.edu:1521:orcl' 'CS347_prof' 'orcl_prof' 'remote';
-# connectTo 'jdbc:oracle:thin:@ib-perfdb.us.oracle.com:1525:perfdb' 'cannata' 'orcl' 'remote';
-for t in SQL """ select table_name from user_tables """ :
-   print t[0]
-   if t[0] == 'EMP' :
-      sql = "select * from " + t[0]
-      print SQL "" sql
